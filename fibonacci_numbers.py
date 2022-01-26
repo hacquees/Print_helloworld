f8a8e17e1dd3ebf056094bf73bc3d165e61ab85d
Ankit Kumar Singh <57264717+hacquees@users.noreply.github.com>
2022-01-26 15:38:07 +0530

    Update fibonacci_numbers.py

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -1,14 +1,11 @@
 #Calculate Fibonacci numbers less than a given number and calculates the sum of all alternate numbers (even numbered) in the generated list
 a=int(input("Please enter a number: "))
 l=[]
-b,c=0,1
-l.extend([b,c])
-print(b)
-print(c)
-for i in range(1,a-2):
-	pre=b
+b=-1
+c=1
+while True:
 	s=b+c
-	if s>a:
+	if s>=a:
 		break
 	l.append(s)
 	print(s)
